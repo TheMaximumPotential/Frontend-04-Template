bd8da7e59be9e7345d3ff33ae3457261e9c0dbcf
Apple <apple@AppletekiiMac.mshome.net>
2020-09-19 17:51:42 +0800

    homework commit

--- Week_04/app.js
+++ Week_04/app.js
@@ -1,20 +1,20 @@
-// const UTF_Encodeing = (string) => {
-// 	let code = encodeURI(string)
-// 	console.log(code, code.length)
-// 	let codeList = []
-// 	let i = 0
-// 	while (i < code.length) {
-// 		if (code[i] === '%') {
-// 			codeList.push(parseInt(code[i + 1] + code[i + 2], 16))
-// 			i += 3
-// 		} else {
-// 			codeList.push(code[i].charCodeAt())
-// 			i++
-// 		}
-// 	}
-// 	return Buffer.from(codeList)
-// }
-// console.log(UTF_Encodeing('今天天气不错'))
+const UTF_Encodeing = (string) => {
+	let code = encodeURI(string)
+	console.log(code, code.length)
+	let codeList = []
+	let i = 0
+	while (i < code.length) {
+		if (code[i] === '%') {
+			codeList.push(parseInt(code[i + 1] + code[i + 2], 16))
+			i += 3
+		} else {
+			codeList.push(code[i].charCodeAt())
+			i++
+		}
+	}
+	return Buffer.from(codeList)
+}
+console.log(UTF_Encodeing('今天天气不错'))
 
 class Dog {
 	constructor(name) {
